58a00717e5e34815ad90d7a1b1b6291abba0d51b
Tenusia <110511640+Tenusia@users.noreply.github.com>
2023-09-22 14:11:29 +0200

    Fix double jump

--- scripts/player.gd
+++ scripts/player.gd
@@ -12,9 +12,11 @@ func _physics_process(delta):
 		if velocity.y > 500:
 			velocity.y = 500
 	
-	if Input.is_action_just_pressed("jump"):
+	if Input.is_action_just_pressed("jump") && is_on_floor():
 		velocity.y = -jump_force
 	
 	var direction = Input.get_axis("move_left", "move_right")
 	velocity.x = direction * speed
 	move_and_slide()
+	
+
