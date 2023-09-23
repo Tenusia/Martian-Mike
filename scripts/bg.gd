61a05afdabd8a44950e41258a0cba18680953c1a
Tenusia <110511640+Tenusia@users.noreply.github.com>
2023-09-23 22:25:08 +0200

    Add starting area

--- scripts/bg.gd
+++ scripts/bg.gd
@@ -10,4 +10,4 @@ func _ready():
 func _process(delta):
 	sprite.region_rect.position += delta * Vector2(scroll_speed, scroll_speed)
 	if sprite.region_rect.position >= Vector2(64, 64):
-		sprite.region_rect = Vector2.ZERO
+		sprite.region_rect.position = Vector2.ZERO
